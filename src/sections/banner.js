4c91805eb668ededf362be9de76a6b7aae18f6d1
elGerardo <gerardoyu54@gmail.com>
2022-10-19 16:40:33 -0700

    deply test

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,25 +1,50 @@
 import global from "../styles/global.module.css";
 import style from "../styles/banner.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import Typed from "typed.js";
+import { useEffect, useRef } from "react";
 
 var Banner = () => {
+  let textRef = useRef(null);
 
-    let content = 
+  useEffect(() => {
+    const typed = new Typed(textRef.current, {
+      strings: ["Developer", "Creative", "Innovator"],
+      startDelay: 300,
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 2000,
+      loop: true,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  let content = (
     <div className={`${global.container} ${style.banner}`}>
-        <div className={`${style.content} ${global.flex_space_between}`}>
-            <img src={require('../assets/images/pic.jpg')} alt="gerardo's pic"/>
-            <div className={`${style.banner_text}`}>
-                <p>Hi, I'm Gerardo
-                a mexican <b>web developer</b>, with creative and innovated <b>solutions</b> for you.</p>
-            </div>
+      <div className={`${style.content} ${global.flex_space_between}`}>
+        <div className={`${style.banner_text}`}>
+          <p></p>
+          <span ref={textRef}></span>
         </div>
-        <div className={`${global.btn_container} ${style.goDown}`}>
-            <a href="#about_me" className={`${global.btn}`}><span><FontAwesomeIcon icon={faArrowDown}/></span></a>
-        </div>
+        <img src={require("../assets/images/pic.jpg")} alt="gerardo's pic" />
+      </div>
+      <div className={`${global.btn_container} ${style.goDown}`}>
+        <a href="#about_me" className={`${global.btn}`}>
+          <span>
+            <FontAwesomeIcon icon={faArrowDown} />
+          </span>
+        </a>
+      </div>
     </div>
-    return ( content )
-
+  );
+  return content;
+  //creative
+  //developer
+  //innovated
 };
 
 export default Banner;
